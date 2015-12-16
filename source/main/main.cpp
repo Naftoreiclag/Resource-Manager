b721e426f885605a33e85ecf63019f99b5d42dc2
naftoreiclag <naftoreiclag@gmail.com>
2015-12-15 16:31:30 -0800

    Test opening file

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -1,6 +1,33 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
+#include <string>
+
+void process(std::string filename) {
+    std::cout << filename << std::endl;
+    std::ifstream infile(filename.c_str());
+    std::string line;
+    while(std::getline(infile, line)) {
+        std::cout << line << std::endl;
+    }
+    infile.close();
+}
 
 int main(int argc, char* argv[]) {
+    if(argc <= 1) {
+        std::cout << 
+        "Compiles a resource project into a load-ready resource package.\n"
+        "\n"
+        "Usage: MACBETH [options] source [destination]\n"
+        "\n"
+        ;
+        
+        return 0;
+    }
+    
+    process(argv[1]);
+    
+    
     std::cout << "Hello World!" << std::endl;
     return 0;
 }
