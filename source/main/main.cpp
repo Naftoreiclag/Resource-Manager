06a7209c2a7af3a1a857e936fce73dc085048b79
naftoreiclag <naftoreiclag@gmail.com>
2015-12-15 17:00:43 -0800

    Add jsoncpp

--- source/main/main.cpp
+++ source/main/main.cpp
@@ -3,14 +3,30 @@
 #include <sstream>
 #include <string>
 
+#include "json/json.h"
+
 void process(std::string filename) {
-    std::cout << filename << std::endl;
+    std::cout << "Processing: " << filename << std::endl;
     std::ifstream infile(filename.c_str());
-    std::string line;
-    while(std::getline(infile, line)) {
-        std::cout << line << std::endl;
-    }
+    Json::Value root;
+    infile >> root;
     infile.close();
+    
+    
+    std::string projectName = root["name"].asString();
+    std::cout << "Project Name: " << projectName << std::endl;
+    
+    /*
+    Json::Value& objects = root["objects"];
+    for(Json::ValueIterator iter = objects.begin(); iter != objects.end(); ++ iter) {
+        Json::Value& objectData = *iter;
+        
+        std::string objectFilename = objectData.asString();
+        
+        std::cout << objectFilename << std::endl;
+    }
+    */
+    
 }
 
 int main(int argc, char* argv[]) {
